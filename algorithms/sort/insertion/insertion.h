34c19c552ad8fd325464cbb8a1733ab505593fff
Nichols <dnicho22@vols.utk.edu>
2018-07-18 23:35:26 -0400

    updated insertion sorting algorithm

---
+++ algorithms/sort/insertion/insertion.h
@@ -0,0 +1,22 @@
+#ifndef _INSERTION_H_
+#define _INSERTION_H_
+
+namespace alg {
+
+template <typename T>
+void insertionSort(std::vector<T>& v) {
+    int i, key, j;
+    for (i = 0; i < v.size(); ++i) {
+        key = v[i];
+        j = i - 1;
+
+        while (j >= 0 && v[j] > key) {
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = key;
+    }
+};
+
+}  // namespace alg
+#endif
--- algorithms/sort/insertion/insertion.md
+++ algorithms/sort/insertion/insertion.md
@@ -2,3 +2,8 @@
 ---------------------------
 
 Insertion sort is an in-place memory swap sorting algorithm that runs in _o_(n^2) time.
+
+
+|           | Worst    | Best   | Average  | Worst-Space |
+| --------- | -------- | ------ | -------- | ----------- |
+| Insertion | _O_(n^2) | _O_(n) | _O_(n^2) | _O_(n)      |
