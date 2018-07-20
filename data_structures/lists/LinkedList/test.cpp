7d05fa740e971e56dd636a51608e2d8537bb0cc7
Nichols <dnicho22@vols.utk.edu>
2018-07-19 23:14:35 -0400

    working test for linked lists

--- data_structures/lists/LinkedList/test.cpp
+++ data_structures/lists/LinkedList/test.cpp
@@ -6,4 +6,12 @@ using namespace dat;
 int main(int argc, char* argv[]) {
     linkedlist<int> l;
     l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+    l.push_front(1);
+    l.push_back(5);
+
+    for (linkedlist<int>::iterator i = l.begin(); i != l.end(); i = i->right) {
+        std::cout << i->val << std::endl;
+    }
 }
