2b828fddf1f46fe1b8fd1d190812f1c7eeb41871
Nichols <dnicho22@vols.utk.edu>
2018-07-19 23:08:41 -0400

    completed a working linked list class (excluding copy operations)

--- data_structures/lists/LinkedList/linkedlist.h
+++ data_structures/lists/LinkedList/linkedlist.h
@@ -4,31 +4,35 @@
 namespace dat {
 
 template <typename T>
-class LLNode {
-    public:
-        LLNode* left;
-        LLNode* right;
-        T val;
-};
-
-template <typename T>
 class linkedlist {
-    public:
+   private:
+    class Node {
+       public:
+        Node* left;
+        Node* right;
+        T val;
+    };
 
-    typedef LLNode<T>* iterator;
+    unsigned int _size;
+    Node* sentinel;
+
+    Node* find(const T& val);
+
+   public:
+    typedef Node* iterator;
 
     linkedlist();
     linkedlist(const linkedlist&);
     ~linkedlist();
+    linkedlist<T>& operator=(const linkedlist&);
 
     void push_back(const T&);
     void push_front(const T&);
-    void insert_after(const T&);
-    void insert_before(const T&);
-    
+    void insert_after(const T&, const T&);
+    void insert_before(const T&, const T&);
+
     T pop_front();
     T pop_back();
-    LLNode<T>* find(T);
 
     iterator begin() { return sentinel->right; }
     iterator end() { return sentinel; }
@@ -37,15 +41,11 @@ class linkedlist {
 
     bool empty() { return _size == 0; }
     unsigned int size() { return _size; }
-
-    private:
-     unsigned int _size;
-     LLNode<T>* sentinel;
 };
 
 // implementation
 template <typename T>
-linkedlist<T>::linkedlist() : _size(0), sentinel(new LLNode<T>) {
+linkedlist<T>::linkedlist() : _size(0), sentinel(new Node) {
     sentinel->right = sentinel;
     sentinel->left = sentinel;
 }
@@ -55,9 +55,111 @@ linkedlist<T>::linkedlist(const linkedlist<T>& copy) : _size(copy.size()) {
     // TODO finish copy constructor
 }
 
-
-
+template <typename T>
+linkedlist<T>& linkedlist<T>::operator=(const linkedlist<T>& val) {
+    // TODO finish copy operator
+    return NULL;
 }
 
+template <typename T>
+linkedlist<T>::~linkedlist() {
+    for (iterator i = begin(); i != end(); i = i->right) delete i;
+    delete sentinel;
+}
+
+template <typename T>
+void linkedlist<T>::push_back(const T& val) {
+    // create the new node with the given value
+    Node* n = new Node;
+    n->val = val;
+
+    // attach new node to right of previous back
+    Node* back = sentinel->left;
+    back->right = n;
+    n->left = back;
+
+    // attach new node to front of sentinel
+    n->right = sentinel;
+    sentinel->left = n;
+    _size++;
+}
+
+template <typename T>
+void linkedlist<T>::push_front(const T& val) {
+    // create the node with the given value
+    Node* n = new Node;
+    n->val = val;
+
+    // attach new node to left of previous front
+    Node* front = sentinel->right;
+    front->left = n;
+    n->right = front;
+
+    // attach new node to right of sentinel
+    n->left = sentinel;
+    sentinel->right = n;
+    _size++;
+}
+
+template <typename T>
+T linkedlist<T>::pop_back() {
+    Node* popped = sentinel->left;
+
+    // connect sentinel with new back
+    popped->left->right = sentinel;
+    sentinel->left = popped->left;
+
+    // delete node as it is being removed
+    T val = popped->val;
+    delete popped;
+
+    return val;
+}
+
+template <typename T>
+T linkedlist<T>::pop_front() {
+    Node* popped = sentinel->right;
+
+    // connect sentinel with new front
+    popped->right->left = sentinel;
+    sentinel->right = popped->right;
+
+    // delete node as it is being removed
+    T val = popped->val;
+    delete popped;
+
+    return val;
+}
+
+template <typename T>
+void linkedlist<T>::insert_after(const T& newVal, const T& leftVal) {
+    Node* ins = new Node;
+    ins->val = newVal;
+    Node* left = find(leftVal);
+
+    ins->right = left->right;
+    left->right = ins;
+    ins->left = left;
+}
+
+template <typename T>
+void linkedlist<T>::insert_before(const T& newVal, const T& rightVal) {
+    Node* ins = new Node;
+    ins->val = rightVal;
+    Node* right = find(rightVal);
+
+    ins->left = right->left;
+    right->left = ins;
+    ins->right = right;
+}
+
+template <typename T>
+typename linkedlist<T>::Node* linkedlist<T>::find(const T& val) {
+    for (iterator i = begin(); i != end(); i = i->right)
+        if (i->val == val) return val;
+    return NULL;
+}
+
+}  // namespace dat
 
 #endif
