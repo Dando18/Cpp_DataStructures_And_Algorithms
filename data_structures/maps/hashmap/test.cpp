1c566cb1f24dec752a504e14d8557457c0b2168d
Daniel Nichols <dando18studios@gmail.com>
2018-07-27 14:10:54 -0400

    operator[] fixes

--- data_structures/maps/hashmap/test.cpp
+++ data_structures/maps/hashmap/test.cpp
@@ -7,5 +7,10 @@ int main(int argc, char* argv[]) {
 
     HashMap<std::string, int> employeeAges;
 
+    employeeAges.add("Jimothy", 5);
+    employeeAges.add("Craig", 6);
+
+    employeeAges["Jimothy"] = 5;
+    std::cout << employeeAges["Craig"] << std::endl;
     return 0;
 }
